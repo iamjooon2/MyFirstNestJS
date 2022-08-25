26da9bcfe67aa91a76780c5a394e85830fdbddbc
iamjooon2 <iamjooon2@gmail.com>
2022-08-25 17:30:12 +0900

    typeOrm 적용

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './board.model';
+
+@Entity() // 테이블로 변환됨
+export class Board extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: BoardStatus;
+}
